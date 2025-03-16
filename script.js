e8662d479070540f37ce82729426697407ab9bee
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-16 13:27:09 +0700

    benerin js

--- script.js
+++ script.js
@@ -29,5 +29,25 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+// document.addEventListener("DOMContentLoaded", () => {
+//     const form = document.getElementById("contact-form");
 
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault(); 
+      
+//         const name = document.getElementById("name").value;
+//         const email = document.getElementById("email").value;
+//         const message = document.getElementById("message").value;
 
+//         const subject = encodeURIComponent(`Message from ${name}`);
+//         const body = encodeURIComponent(
+//             `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+//         );
+
+//         const mailtoLink = `mailto:yonaldiernandaputro@gmail.com?subject=${subject}&body=${body}`;
+
+//         window.location.href = mailtoLink;
+
+//         form.reset();
+//     });
+// });
