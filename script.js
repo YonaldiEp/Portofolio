cd4ad4553c715e3faa18755c2eda745bb75c6e58
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-16 13:30:07 +0700

    fix js

--- script.js
+++ script.js
@@ -27,27 +27,3 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset();
     });
 });
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const form = document.getElementById("contact-form");
-
-//     form.addEventListener("submit", (event) => {
-//         event.preventDefault(); 
-      
-//         const name = document.getElementById("name").value;
-//         const email = document.getElementById("email").value;
-//         const message = document.getElementById("message").value;
-
-//         const subject = encodeURIComponent(`Message from ${name}`);
-//         const body = encodeURIComponent(
-//             `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
-//         );
-
-//         const mailtoLink = `mailto:yonaldiernandaputro@gmail.com?subject=${subject}&body=${body}`;
-
-//         window.location.href = mailtoLink;
-
-//         form.reset();
-//     });
-// });
