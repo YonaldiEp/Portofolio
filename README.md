f255444717fcd3249f484a8ec10d0be808903012
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-17 20:44:43 +0700

    update

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Portofolio Website ini dibuat menggunakan HTML, CSS, dan JavaScript dengan fokus pada desain yang responsif dan minimalis. Proyek ini mencakup berbagai fitur seperti Home, About, Projects, dan Contact.
 
+## Struktur Proyek:
+
+- **HTML**: Menyediakan struktur dasar dari halaman web.
+- **CSS**: Mengatur tata letak, warna, font, dan animasi.
+- **JavaScript**: Menambahkan interaktivitas seperti toggle, smooth scrolling, dan burger menu.
+
 ## Link GitHub Pages:
 
 Anda dapat mengakses website ini dengan link berikut:
--- script.js
+++ script.js
@@ -28,33 +28,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// const menuIcon = document.querySelector('#menu-icon');
-// const navLinks = document.querySelector('.nav-links');
-
-// menuIcon.onclick = () => {
-//     navLinks.classList.toggle('active')
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const form = document.getElementById("contact-form");
-
-//     form.addEventListener("submit", (event) => {
-//         event.preventDefault(); 
-      
-//         const name = document.getElementById("name").value;
-//         const email = document.getElementById("email").value;
-//         const message = document.getElementById("message").value;
-
-//         const subject = encodeURIComponent(`Message from ${name}`);
-//         const body = encodeURIComponent(
-//             `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
-//         );
-
-//         const mailtoLink = `mailto:yonaldiernandaputro@gmail.com?subject=${subject}&body=${body}`;
-
-//         window.location.href = mailtoLink;
-
-//         form.reset();
-//     });
-// });
-
