1f0a7687f9fa8ee856acfcaab9a925137c9d21b0
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-17 20:19:46 +0700

    tambah js

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset();
     });
 });
+
+// const menuIcon = document.querySelector('#menu-icon');
+// const navLinks = document.querySelector('.nav-links');
+
+// menuIcon.onclick = () => {
+//     navLinks.classList.toggle('active')
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const form = document.getElementById("contact-form");
+
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault(); 
+      
+//         const name = document.getElementById("name").value;
+//         const email = document.getElementById("email").value;
+//         const message = document.getElementById("message").value;
+
+//         const subject = encodeURIComponent(`Message from ${name}`);
+//         const body = encodeURIComponent(
+//             `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+//         );
+
+//         const mailtoLink = `mailto:yonaldiernandaputro@gmail.com?subject=${subject}&body=${body}`;
+
+//         window.location.href = mailtoLink;
+
+//         form.reset();
+//     });
+// });
+
