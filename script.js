52de34b26a1c39d0559099060638f9ecfb638c83
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-18 13:12:31 +0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
+
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active')
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("contact-form");
