9bd3d5f80d4711fe53402731a91c6d4ddc87a382
YonaldiEp <yonaldiernandaputro@gmail.com>
2025-03-22 21:31:50 +0700

    update

--- script.js
+++ script.js
@@ -21,10 +21,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('.nav-links');
-
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active')
-}
 
